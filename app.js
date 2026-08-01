739f779de856074629e417cd7e9d2aba8c13df7a
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-08-01 19:08:15 +0530

    Add weather data display elements and update getWeather function to show visibility

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const temperature = document.getElementById("temperature");
 const description = document.getElementById("description");
 const humidity = document.getElementById("humidity");
 const wind = document.getElementById("wind");
+const weatherIcon = document.getElementById("weatherIcon");
+const feelsLike = document.getElementById("feelsLike");
+const pressure = document.getElementById("pressure");
+const visibility = document.getElementById("visibility");
 
 let errorMessage = document.getElementById("errorMessage");
 
@@ -40,11 +44,20 @@ async function getWeather(city) {
 
         let data = await response.json();
 
-        console.log(data.name);
-        console.log(data.main.temp);
-        console.log(data.main.humidity);
-        console.log(data.wind.speed);
-        
+        cityName.textContent = data.name;
+        temperature.textContent = `${data.main.temp}°C`;
+        description.textContent = data.weather[0].description;
+        humidity.textContent = `${data.main.humidity}--%`;
+        wind.textContent = `${data.wind.speed}-- km/h`;
+
+        let iconCode = data.weather[0].icon;
+        let iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+        weatherIcon.src = iconUrl;
+
+        feelsLike.textContent = `${data.main.feels_like}°C`;
+        pressure.textContent = `${data.main.pressure} hPa`;
+        visibility.textContent = `${data.visibility / 1000}Km`;
 
     } catch (error) {
         console.log(error.message);
